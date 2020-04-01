77cde7ec6735feac6e360a3d3a0056eeea11b814
hoang.nguyen <hoang.nguyen@uzabase.com>
2020-04-01 22:22:17 +0900

    new answer to 3.rb

--- class/20200322/3.rb
+++ class/20200322/3.rb
@@ -2,14 +2,16 @@ n = 10
 for i in 1..n do 
 	if i == 1 or i == 10 then
 		for k in 1..n do
-			print("*")
+			print("* ")
 		end
 	else
 		for k in 1..n do
-			if k == 1 or k== 10
+			if k == 1
 				print("*")
+			elsif k == 10
+				print(" *")
 			else
-				print(" ")
+				print("  ")
 			end
 		end
 	end
